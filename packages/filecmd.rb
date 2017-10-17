a860f5406f6ce73a0266a368698a9db9698f9d36
Fred Clift <fred@clift.org>
2017-10-16 23:23:04 -0600

    add x86_64 binary package

--- packages/filecmd.rb
+++ packages/filecmd.rb
@@ -8,8 +8,10 @@ class Filecmd < Package
   source_sha256 '8639dc4d1b21e232285cd483604afc4a6ee810710e00e579dbe9591681722b50'
 
   binary_url ({
+    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/filecmd-5.32-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    x86_64: 'b016618a9f661bfef469a21d78cd66312de376dc10a32f8598112c20459542b2',
   })
 
   def self.build
